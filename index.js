55225d2d1e65e6aaa052b33d83f99b7e0404ba81
Ruslan Abibullaev <ruslan_abibullaev@mail.ru>
2025-12-09 18:35:35 +0500

    Complete app functionality

---
+++ index.js
@@ -0,0 +1,33 @@
+const colorInput = document.querySelector('#color-input');
+const modeSelect = document.querySelector('#mode-select');
+const getSchemeBtn = document.querySelector('#get-scheme-btn');
+
+getSchemeBtn.addEventListener('click', getColorData);
+
+function getColorData() {
+  const inputVal = colorInput.value.slice(1);
+  const selectVal = modeSelect.value;
+
+  fetch(`https://www.thecolorapi.com/scheme?hex=${inputVal}&mode=${selectVal}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const colorData = data.colors.reduce((arr, color) => {
+        arr.push(color.hex.value);
+        return arr;
+      }, []);
+
+      renderColors(colorData);
+    });
+}
+
+function renderColors(colorArr) {
+  const colorBgEls = document.querySelectorAll('.color-bg');
+  const colorCodeEls = document.querySelectorAll('.color-code');
+
+  colorBgEls.forEach((el, i) => {
+    el.style.backgroundColor = colorArr[i];
+    colorCodeEls[i].textContent = colorArr[i];
+  });
+}
+
+window.addEventListener('load', getColorData());
